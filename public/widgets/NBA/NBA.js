83ec972a77954e9d605e3682673724b397082452
Vesa Frijling <vesaf@msn.com>
2018-06-16 15:33:53 +0200

    Deleted widgets folder

--- public/widgets/NBA/NBA.js
+++
@@ -1,7 +0,0 @@
-var clockInterval
-window.addEventListener("load", function() {
-    var css = '<link rel="stylesheet" type="text/css" href="widgets/NBA/style.css">';
-    var html = ``;
-    initializeWidget("NBA", html, css);
-});
-
